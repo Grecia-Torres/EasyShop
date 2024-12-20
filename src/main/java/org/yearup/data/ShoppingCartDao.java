1260200f6051f6a8a3e2c0716697b2d95b5db636
Grecia-Torres <grecia.atorres@gmail.com>
2024-12-19 23:04:39 -0800

    updated ShoppingCartController

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,12 @@
 package org.yearup.data;
 
-import org.yearup.models.ShoppingCart;
+import org.yearup.models.*;
 
-public interface ShoppingCartDao
-{
+public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
+
+    ShoppingCart add(int userId, int productId, int quantity);
+
+    ShoppingCart emptyCart(int userId);
     // add additional method signatures here
 }
